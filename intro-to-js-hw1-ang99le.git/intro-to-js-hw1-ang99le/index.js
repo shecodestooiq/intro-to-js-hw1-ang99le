c215d4831931f646c251a1bcd03986a1568fdef2
Angel <75809960+ang99le@users.noreply.github.com>
2024-02-05 02:45:11 +0300

    Add files via upload

---
+++ intro-to-js-hw1-ang99le.git/intro-to-js-hw1-ang99le/index.js
@@ -0,0 +1,19 @@
+//Task1 
+
+var Name="malak"
+var age=24
+var isStudent="true"
+
+console.log(Name);
+console.log(age);
+console.log(isStudent);
+
+//Task2
+var variableTocheck="Salama"
+console.log("The type of data is :", typeof variableTocheck)
+
+// Task3
+var greeting="welcome"
+var myName="Malak"
+console.log("I would like to say:" + greeting,myName);
+
